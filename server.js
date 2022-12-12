7b874bb252db4e5921a08df6c4666911cb7956df
hannybear88 <hannahkchung88@gmail.com>
2022-12-12 13:05:38 -0800

    changed port number also added code for server.js in previous commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const path = require('path');
 const app = express();
 
 // Sets an initial port. We'll use this later in our listener
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3002;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
